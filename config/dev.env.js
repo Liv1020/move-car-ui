ad24163be46f82c80c6a5b4eb8fcc4781f37e7f2
Pavle Lee <523260513@qq.com>
2018-07-01 20:13:33 +0800

    init

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,5 +5,5 @@ module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   HOST: '"http://mc.liv1020.com"',
   API_HOST: '"http://localhost:8080/frontend"',
-  WEB_SOCKET: '"wx://localhost:8080/frontend/ws"'
+  WEB_SOCKET: '"ws://localhost:8080/frontend/ws"'
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -2,5 +2,5 @@ module.exports = {
   NODE_ENV: '"production"',
   HOST: '"http://mc.liv1020.com"',
   API_HOST: '"http://47.93.19.226:8080/frontend"',
-  WEB_SOCKET: '"wx://47.93.19.226:8080/frontend/ws"'
+  WEB_SOCKET: '"ws://47.93.19.226:8080/frontend/ws"'
 }
